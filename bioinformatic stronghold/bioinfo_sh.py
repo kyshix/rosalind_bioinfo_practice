b06b8ef011d0a467d04bbf8e3915afedce7e17c1
Kitty Shi <shikitty.023@gmail.com>
2026-01-06 23:55:13 -0500

    completed dna nt counting, transcription to mrna, and dna reverse complement functions

--- bioinformatic stronghold/bioinfo_sh.py
+++ bioinformatic stronghold/bioinfo_sh.py
@@ -29,8 +29,67 @@ def count_dna(fname):
         
         # METHOD #2
         counts = Counter(dna)
-        print(f"{counts["A"]} {counts["C"]} {counts["G"]} {counts["T"]}")
+        return(f"{counts["A"]} {counts["C"]} {counts["G"]} {counts["T"]}")
                 
+
+# TRANSCRIBING DNA INTO RNA
+"""
+Transcribe DNA Sequence into Corresponding mRNA
+:param fname: file containing a DNA string t with length of at most 1000 nt
+:returns: transcribed mRNA string of t
+"""
+def transcribe_to_mrna(fname): 
+    # convert all thymine nt to uracil nt 
+    with open(fname, "r") as file:
+        dna = file.read()
+        mRNA = dna.replace("T", "U")
+    return mRNA
+
+
+# THE SECONDARY AND TETIARY STRUCTURE OF DNA
+# secondary structure of dna refers to the double helix in which complementary strands
+# run antiparallel of each other with base pariings A-T and G-C (storage of genetic info)
+# tetiary structure of dna refers to the 3D arrangement of the double helix that allows
+# you to understand folding/coiling patterns (access to genetic info)
+
+# reverse complement counterpart represents the sequence of DNA strand (only one direction
+# because of how dna synthesis occurs)
+# this is essential for identifying potential binding sites, matching sequences, etc. 
+"""
+Reverse Complement Counterpart of DNA Strand
+:param fname: file containing DNA string s of length at most 1000 bp (5'-3' strand)
+:results: reverse complement strand to s (5'-3' strand)
+"""
+def reverse_complement_dna(fname): 
+    with open(fname, "r") as file: 
+        rev_dna = file.read()[::-1]
         
+        # METHOD 1
+        # rev_comp_dna = ""
+        # for nt in rev_dna:
+        #     if nt == "A": 
+        #         rev_comp_dna += "T"
+        #     elif nt == "C": 
+        #         rev_comp_dna += "G"
+        #     elif nt == "G": 
+        #         rev_comp_dna += "C"
+        #     elif nt == "T": 
+        #         rev_comp_dna += "A"
+        # return rev_comp_dna
+        
+        # METHOD 2
+        bp_translation = str.maketrans("ACGT", "TGCA")
+        rev_compl_dna = rev_dna.translate(bp_translation)
+        return rev_compl_dna
+
+
+# RABBIT AND RECURRENCE RELATIONS
+# recurrance relation: refer to justin's recurrence relation notes
+"""
+Reverse
+"""
+
 if __name__ == "__main__":
-    count_dna("rosalind.txt")
+    print(count_dna("rosalind_dna.txt"))
+    print(transcribe_to_mrna("rosalind_rna.txt"))
+    print(reverse_complement_dna("rosalind_revc.txt"))
